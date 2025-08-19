3b05b7154d45492339ab822fcb0a150aa7f3d9b8
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-08-19 09:43:31 +0530

    Code With Mosh ( Games )
    This are the python function That Learn form mosh and Developed together

---
+++ CWM - Guess The Number Game.py
@@ -0,0 +1,14 @@
+import random
+
+secrect_number = random.randint(1,10) 
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit :
+    guess = int(input("Enter Your Guess Number : "))
+    guess_count +=1 
+    if guess == secrect_number:
+        print (" You Won The Game ")
+        break
+else:
+    print(" Hey You Guess 3 Times.... ")
+    print(" Try Next Time... ")
---
+++ CWm - Car Game.py
@@ -0,0 +1,32 @@
+command = ""
+
+started = False
+
+while True:
+    command = input ("> ").lower()
+
+    if command == "help" :
+        print(" start - to start the car ")
+        print(" stop - to stop the car ")
+        print(" quit - to exit ")
+
+    elif command == "start" :
+        if started:
+            print("Car is alreday Started!")
+        else:
+            started = True    
+            print ("Car Started...Ready to go!")
+
+    elif  command == "stop" : 
+        if not started:
+            print("Car is Already stopped!")
+        else:
+            started = False
+            print ("Car Stopped.")
+
+    elif command == "quit" :
+        print ("You are Exit Form the Game...")
+        break
+
+    else :
+        print ("Invalid Commend... ")
